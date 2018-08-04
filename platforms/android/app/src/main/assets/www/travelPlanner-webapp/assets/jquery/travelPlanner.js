b6d51100fab87e1325c1d44df526bf60ec2daf0d
rubtavpic <rubentavero@outlook.com>
2018-08-04 13:05:55 +0200

    Proyecto actualizado usando cordova

---
+++ platforms/android/app/src/main/assets/www/travelPlanner-webapp/assets/jquery/travelPlanner.js
@@ -0,0 +1,9 @@
+function mostrarCamposSesion(){      
+     $("#btns_inicio").slideUp('slow');
+    $("#camposSesion").slideDown('slow');  
+}
+
+function mostrarOlvidarContrasenia(){      
+    $("#camposSesion").slideUp('slow');
+   $("#olvidar_contrasenia").slideDown('slow');  
+}
--- src/app/components/registro.component.ts
+++ src/app/components/registro.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var device;
 
 @Component({
 	selector: 'registro',
 	templateUrl: '../views/registro.html'
 })
-export class RegistroComponent{
+export class RegistroComponent implements OnInit{
 	public titulo: string;
 
 	constructor(){		
+	}
+
+	ngOnInit(){
+		document.addEventListener("deviceready", function(){alert(device.platform);}, false);
 	}
 }
