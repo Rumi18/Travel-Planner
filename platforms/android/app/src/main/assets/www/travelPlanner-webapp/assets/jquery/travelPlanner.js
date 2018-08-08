ddefe11afcb75ba3c6f137176d5ff64a7e7dd00b
rubtavpic <rubentavero@outlook.com>
2018-08-08 18:40:53 +0200

    Proyecto vacío

--- platforms/android/app/src/main/assets/www/travelPlanner-webapp/assets/jquery/travelPlanner.js
+++
@@ -1,9 +0,0 @@
-function mostrarCamposSesion(){      
-     $("#btns_inicio").slideUp('slow');
-    $("#camposSesion").slideDown('slow');  
-}
-
-function mostrarOlvidarContrasenia(){      
-    $("#camposSesion").slideUp('slow');
-   $("#olvidar_contrasenia").slideDown('slow');  
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { routing, appRoutingProviders } from './app.routing';
 
-// Imports para el idioma
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-// Componentes
 import { AppComponent } from './app.component';
-import { InicioComponent } from './components/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InicioComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    routing,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    })
+    BrowserModule
   ],
-  providers: [appRoutingProviders],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-export function createTranslateLoader(http: HttpClient){
-  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
-}
